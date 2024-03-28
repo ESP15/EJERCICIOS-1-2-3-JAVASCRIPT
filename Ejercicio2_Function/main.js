df81725a1a6e582bc6bd6299615398f1a5043b9a
Eric Sanz <160590921+ESP15@users.noreply.github.com>
2024-03-28 11:57:49 +0100

    Add files via upload

---
+++ Ejercicio2_Function/main.js
@@ -0,0 +1,32 @@
+// Ejercicio 1
+console.log("Ejercicio1")
+function numero(num1,num2,num3){
+    return(num1+num2+num3);
+}
+
+console.log(numero(1,3,5));
+
+// Ejercicio 2
+console.log("Ejercicio2")
+function nombreCompleto(nombre, apellido, apellido2){
+    return (nombre+" " + apellido +" "+apellido2)
+}
+console.log(nombreCompleto("Eric","Sanz","Peiri"))
+
+// Ejercicio 3
+console.log("Ejercicio3")
+function numMayor(num1,num2){
+    if(num1>=num2 ){
+        console.log(num1);
+    } else {
+        console.log(num2);
+    }
+}
+numMayor(5,4);  
+  
+
+
+
+
+
+
